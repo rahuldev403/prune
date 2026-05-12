ad71e07a146986ef440ce5fc4c4a61ea23627978
Rahul Swain <rahuls.403.dev@gmail.com>
2026-05-12 13:10:45 +0530

    fix: handle missing DATABASE_URL by providing a default connection string

--- index.ts
+++ index.ts
@@ -2,10 +2,8 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./db/schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("Database url is missing. Cheack your .env file.");
-}
-
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(
+  process.env.DATABASE_URL || "postgres://dummy:dummy@dummy/dummy",
+);
 
 export const db = drizzle(sql, { schema });
